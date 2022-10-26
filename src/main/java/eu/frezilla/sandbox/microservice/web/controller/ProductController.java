95d62c6e19b037e4802b5994cf66c39fdf849ff7
frezilla <frederic.balme@laposte.net>
2022-10-26 13:21:51 +0200

    Implémentation des méthodes POST

--- src/main/java/eu/frezilla/sandbox/microservice/web/controller/ProductController.java
+++ src/main/java/eu/frezilla/sandbox/microservice/web/controller/ProductController.java
@@ -5,25 +5,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import eu.frezilla.sandbox.microservice.dao.ProductRepository;
+import java.net.URI;
 import java.util.List;
+import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class ProductController {
-    
-    @Autowired private ProductRepository productDao;
-    
+
+    @Autowired
+    private ProductRepository productDao;
+ 
+    @PostMapping(value = "/products")
+     public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+         Product productSaved = productDao.save(product);
+         if (Objects.isNull(productSaved)) {
+             return ResponseEntity.noContent().build();
+         }
+         URI location = ServletUriComponentsBuilder
+                 .fromCurrentRequest()
+                 .path("/{id}")
+                 .buildAndExpand(productSaved.getId())
+                 .toUri();
+         return ResponseEntity.created(location).build();
+     }
     
     @GetMapping("products/{id}")
     public Product getProductById(@PathVariable int id) {
         return productDao.findById(id).orElse(null);
     }
-    
+
     @GetMapping("/products")
     public List<Product> getProducts() {
         return productDao.findAll();
     }
-    
-    
-    
+
 }
